9cc6257b25f6f7d6b5a77bc2f836276fefab4d92
midoks <midoks@163.com>
2015-08-24 00:43:01 +0800

    完成横竖向的icon列表

--- EampleApp/EampleApp/Controllers/Main/MainViewController.swift
+++ EampleApp/EampleApp/Controllers/Main/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController, PictureSwitchingDelegate,
+class MainViewController: UIViewController, PictureSwitchingDelegate, MDCrossRangeIconDelegate,
 UIScrollViewDelegate, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
     
     var main: UIScrollView?
@@ -55,16 +55,16 @@ UIScrollViewDelegate, UISearchBarDelegate, UITableViewDelegate, UITableViewDataS
         self.mainSubviewCurrentHeight = 0
         self.mainHeight = 0
         self.mainHeight = self.getMainHeight()
-
+        
         main = UIScrollView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
         main!.contentSize = CGSizeMake(self.view.frame.width, self.mainHeight!)
         main!.backgroundColor = UIColor(red: 239/255, green: 239/255, blue: 244/255, alpha: 1)
-        main!.showsVerticalScrollIndicator = true
+        main!.showsVerticalScrollIndicator = false
         main!.showsHorizontalScrollIndicator = false
         main!.scrollEnabled = true
         main!.delegate = self
-
-
+        
+        
         self.view.addSubview(main!)
     }
     
@@ -123,93 +123,49 @@ UIScrollViewDelegate, UISearchBarDelegate, UITableViewDelegate, UITableViewDataS
     //MARK: - 分类列表 -
     func initCategoryList(){
         let pos = self.mainSubviewCurrentHeight!
-        self.mainSubviewCurrentHeight = self.mainSubviewCurrentHeight! + 100
+        self.mainSubviewCurrentHeight = self.mainSubviewCurrentHeight! + 240 + 5
         self.setMainHeight(self.mainSubviewCurrentHeight!)
         
+        print(pos)
+        print(self.mainSubviewCurrentHeight)
+        
+        let menu = MDCrossRangeIcon(frame: CGRectMake(0, pos + 5, self.view.frame.width, 240))
+        menu.delegate = self
         
         
-        let viewIconWidth = self.view.frame.width / 4
-        let viewIconHeight = 70.0
-
+        let t = UIImage(imageLiteral: "img_menu_qrcode")
         
-        let menu = UIView(frame: CGRect(x: 0, y: pos, width: self.view.frame.width, height: 100))
-        //menu.backgroundColor = UIColor.blueColor()
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "第一个标题")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "第二个标题")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "第三个标题")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
         
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
         
-        //print(viewIconWidth)
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
+        menu.addImage(UIImage(named: "img_menu_water.png")!, title: "测试")
+        menu.addImage(t!, title: "OK")
         
-        for(var i=0; i<4; i++){
-            let menuIconView = UIView(frame: CGRect(x: viewIconWidth * CGFloat(i), y: 0, width: viewIconWidth, height: CGFloat(viewIconHeight)))
-            //menuIconView.backgroundColor = UIColor.greenColor()
-            menu.addSubview(menuIconView)
-         
-         
-            let menuIconButton = UIButton(frame: CGRect(x: 0, y: 0, width: viewIconWidth, height: 58))
-            menuIconButton.center = CGPoint(x: viewIconWidth/2, y: CGFloat(viewIconHeight/2))
-            menuIconButton.backgroundColor = UIColor.whiteColor()
-            
-            menuIconButton.setTitle("测试", forState: UIControlState.Normal)
-            menuIconButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
-            menuIconButton.tintColor = UIColor.grayColor()
-            
-            let bgImage = self.imageWithColor(UIColor(red: 239/255, green: 239/255, blue: 244/255, alpha: 1),
-                size: CGSize(width: 58, height: 58))
-
-            menuIconButton.setBackgroundImage(bgImage, forState: UIControlState.Highlighted)
-            //menuIconButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Application)
-            
-            
-            menuIconButton.addTarget(self, action: Selector("t:"), forControlEvents: UIControlEvents.TouchUpInside)
-            
-            
-            
-//            let menuIconImgView = UIImageView(frame: CGRect(x: 0, y: 0, width: 58, height: 58))
-//            let menuIconImg = UIImage(named: "img_menu_water.png")
-//            
-//            menuIconImgView.frame.size.height = menuIconImg!.size.height
-//            menuIconImgView.center = CGPoint(x: viewIconWidth/2, y: CGFloat(viewIconHeight/2))
-//            menuIconImgView.image = menuIconImg
-        
-            
-            menuIconView.addSubview(menuIconButton)
-            
-            
-//            let menuSpliterLine = UIView(frame: CGRect(x: viewIconWidth * CGFloat(i), y: 0, width: 1, height: 100))
-//            menuSpliterLine.backgroundColor = UIColor.grayColor()
-//            menu.addSubview(menuSpliterLine)
-            
-        
-            
-        }
-        
-        
+        menu.render()
         self.main!.addSubview(menu)
     }
     
-    func t(sender:UIButton){
-        print("tt")
-    }
-    
-    //生成纯色背景
-    func imageWithColor(color:UIColor, size: CGSize) -> UIImage {
-        let rect = CGRectMake(0, 0, size.height, size.height)
-        
-        UIGraphicsBeginImageContext(rect.size)
-        
-        let context = UIGraphicsGetCurrentContext()
-        CGContextSetFillColorWithColor(context, color.CGColor)
-        CGContextFillRect(context, rect)
-        
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return image
+    //MARK: - 分类列表代理 -
+    func MDCrossRangeIconTouch(index: Int) {
+        //print(index)
+        let s = String(index)
+        let alertV = UIAlertView(title: "您的结果", message: s, delegate: nil, cancelButtonTitle: "我知道了")
+        alertV.show()
     }
     
     //MARK: - TableView Init -
     func initTableView(){
         
-        let pos = self.mainSubviewCurrentHeight!
+        //let pos = self.mainSubviewCurrentHeight!
         self.mainSubviewCurrentHeight = self.mainSubviewCurrentHeight! + 100
         self.setMainHeight(self.mainSubviewCurrentHeight!)
         
@@ -227,7 +183,7 @@ UIScrollViewDelegate, UISearchBarDelegate, UITableViewDelegate, UITableViewDataS
         
         self.main!.addSubview(initTv)
     }
-
+    
     //MARK: - UITableViewDelegate -
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
---
+++ EampleApp/EampleApp/Framework/MDCrossRangeIcon/MDCrossRangeIcon.swift
@@ -0,0 +1,148 @@
+//
+//  MDCrossRangeIcon.swift
+//
+//  Created by midoks on 15/8/23.
+//  Copyright © 2015年 midoks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct  MDCrossRangList{
+    var image: UIImage?
+    var title: NSString?
+}
+
+class MDCrossRangeIcon: UIView {
+    
+    var icon:MDCrossRangList?
+    var iconList:Array<MDCrossRangList> = Array<MDCrossRangList>()
+    var delegate: MDCrossRangeIconDelegate?
+    
+    var column: Int = 4
+    var rowHeight: CGFloat = 80
+    
+    //MARK: - 初始化 -
+    override init(frame:CGRect){
+        super.init(frame:frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - 添加图标和标题 -
+    func addImage(image:UIImage, title: NSString){
+        let item = MDCrossRangList(image: image, title: title)
+        self.iconList.append(item)
+    }
+    
+    //设置横向固定列
+    func setFixedColumn(number:Int){
+        self.column = number
+    }
+    
+    func setIconHeight(height:CGFloat){
+        self.rowHeight = height
+    }
+    
+    func addFillIcon(number: Int){
+        let num = number - self.iconList.count
+        if(num > 0){
+            for(var i = 0; i < num; i++){
+                self.addImage(UIImage(), title: "")
+            }
+        }
+    }
+    
+    //MARK: - 显示结构 -
+    func render(){
+        
+        let column  =   self.column
+        let row     =   Int(self.frame.height/self.rowHeight)
+        self.addFillIcon(column * row)
+        
+        let viewIconWidth = self.frame.width / CGFloat(column)
+        let viewIconHeight = self.rowHeight
+    
+        var num = 0
+        for(var i = 0; i < row; i++){
+            for(var j = 0; j < column; j++){
+                if(self.iconList.count > num){
+                    //print(num)
+                    let value = self.iconList[num]
+                    self.addIconItem(value, viewIconWidth: viewIconWidth, viewIconHeight: viewIconHeight,
+                        j: CGFloat(j), i: CGFloat(i), num: num)
+                }
+//                else{
+//                    self.addImage(UIImage(named: "img_menu_water.png")!, title: "11")
+//                }
+                num++
+            }
+        }
+    }
+    
+    //MARK: - 添加 -
+    private func addIconItem(value: MDCrossRangList, viewIconWidth: CGFloat, viewIconHeight: CGFloat,
+            j:CGFloat, i:CGFloat, num:Int){
+    
+        let menuIconItem = UIButton(frame: CGRect(x: viewIconWidth * j, y: 0.5 + i*self.rowHeight,
+            width: viewIconWidth - 0.5, height: CGFloat(viewIconHeight) - 0.5))
+        menuIconItem.backgroundColor =  UIColor.whiteColor()
+        
+        let bgImage = self.imageWithColor(UIColor(red: 239/255, green: 239/255, blue: 244/255, alpha: 1),
+            size: CGSize(width: 58, height: 58))
+        menuIconItem.setBackgroundImage(bgImage, forState: UIControlState.Highlighted)
+        menuIconItem.tag = num
+        menuIconItem.addTarget(self, action: Selector("itemClick:"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
+        //图片
+        let menuIconImgView = UIImageView(frame: CGRect(x: 0, y: 0, width: 29, height: 29))
+        let menuIconImg = value.image
+        menuIconImgView.center = CGPoint(x: viewIconWidth/2, y: CGFloat(viewIconHeight/2) - 10)
+        menuIconImgView.image = menuIconImg
+        menuIconItem.addSubview(menuIconImgView)
+        
+        //文字
+        let title = UILabel(frame: CGRectMake(0, 0, viewIconWidth, 30))
+        title.textAlignment = NSTextAlignment.Center
+        title.frame.origin.y = CGFloat(viewIconHeight/2)+5
+        title.textColor = UIColor.grayColor()
+        title.font = UIFont(name: "Arial-BoldItalicMT", size: 12)
+        title.text = value.title as? String
+        menuIconItem.addSubview(title)
+                
+        self.addSubview(menuIconItem)
+    }
+    
+    
+    func itemClick(sender:UIButton){
+        let pos = sender.tag
+        self.delegate?.MDCrossRangeIconTouch!(pos)
+    }
+    
+    //MARK: - Private Methods -
+    //生成纯色背景
+    func imageWithColor(color:UIColor, size: CGSize) -> UIImage {
+        let rect = CGRectMake(0, 0, size.height, size.height)
+        
+        UIGraphicsBeginImageContext(rect.size)
+        
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetFillColorWithColor(context, color.CGColor)
+        CGContextFillRect(context, rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+    
+}
+
+@objc protocol MDCrossRangeIconDelegate {
+    
+    optional func MDCrossRangeIconTouch(index:Int)
+}
